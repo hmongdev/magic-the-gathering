1fc5de52216956537598587c7347010295e14c0e
Avery Yang <averyyang2674@gmail.com>
2022-05-25 10:37:36 -0500

    center h1

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
-// storing all the panel elements in array: panels
-const lands = document.querySelectorAll('.land');
-
-// forEach = higher-order method that accepts functions as arguments
-// purpose: add eventListener to each panel that expands when clicked
-lands.forEach(land => {
-    land.addEventListener('click', () => {
-        removeActiveClasses();
-        land.classList.add('active');
-    })
-});
-
-// purpose: removes active classes so they collapse when clicked
-const removeActiveClasses = () => {
-    lands.forEach(land => {
-        land.classList.remove('active');
-    })
-}
+const lands = $('.land');
+console.log(lands); //NodeList

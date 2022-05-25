1ab77591974874208640389078c241b8765bee45
Avery Yang <averyyang2674@gmail.com>
2022-05-25 11:14:46 -0500

    removed jQuery tag

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const lands = $('.land');
-console.log(lands); //NodeList
+// storing all the land elements in array: panels
+const lands = document.querySelectorAll('.land');
+console.log(lands);
+
+// forEach = higher-order method that accepts functions as arguments
+// purpose: add eventListener to each land that expands when clicked
+lands.forEach(land => {
+    land.addEventListener('click', () => {
+        removeActiveClasses();
+        land.classList.add('active');
+    })
+});
+
+// purpose: removes active classes so they collapse when clicked
+const removeActiveClasses = () => {
+    lands.forEach(land => {
+        land.classList.remove('active');
+    })
+}
